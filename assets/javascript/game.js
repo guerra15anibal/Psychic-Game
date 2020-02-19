a24c1b94863a7e60892d81c8039a1093c0cb2636
Anibal <guerra15anibal@hotmail.com>
2020-02-18 20:24:57 -0700

    adding Rando function and if else statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,4 +10,36 @@ var randoLetters = randoLetters;
 //Choosing rando letter//
 randoLetters = letters[Math.floor(Math.random() * letters.length)];
 console.log(randoLetters)
+//Choosing rando letter//
 
+//Making rando letter a function//
+function Rando() {
+    randoLetters = letters[Math.floor(Math.random() * letters.length)];
+    console.log(randoLetters)
+}
+//Making rando letter a function//
+
+//.On elements//
+document.onkeyup = function (press) {
+    var userGuess = event.key
+//.On elements//
+
+//Winning if//
+if(userGuess === randoLetters){
+    wins++
+    chances = 9;
+    usedLetters =[];
+}
+//Winning if//
+
+//Using 1 chance every letter pressed//
+Rando();
+if (userGuess !== randoLetters){
+    chances--;
+}
+//Using 1 chance every letter pressed//
+
+
+
+
+}
