9f18c3100de1a85ba92330812762a4a8252961cb
Anibal <guerra15anibal@hotmail.com>
2020-02-18 22:53:02 -0700

    tried to fix issue with unwanted keyups...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Variables//
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","z"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "z"]
 var wins = 0;
 var losses = 0;
 var chances = 10;
@@ -22,44 +22,44 @@ function Rando() {
 //.On elements//
 document.onkeyup = function (press) {
     var userGuess = event.key
-//.On elements//
+    //.On elements//
 
-//Winning//
-if(userGuess === randoLetters){
-    wins++
-    chances = 10;
-    usedLetters =[];
-}
-//Winning//
+    //Winning//
+    if (userGuess === randoLetters) {
+        wins++
+        chances = 10;
+        usedLetters = [];
+    }
+    //Winning//
 
-//Using 1 chance every letter pressed//
-Rando();
-if (userGuess !== randoLetters){
-    chances--;
-}
-//Using 1 chance every letter pressed//
+    //Using 1 chance every letter pressed//
+    Rando();
+    if (userGuess !== randoLetters) {
+        chances--;
+    }
+    //Using 1 chance every letter pressed//
 
-//Losing//
-if(chances ==0){
-    losses++
-    usedLetters= []
-    chances = 10;
-}
-//Losing//
+    //Losing//
+    if (chances == 0) {
+        losses++
+        usedLetters = []
+        chances = 10;
+    }
+    //Losing//
 
-//Assighs variables to IDs//
-if (usedLetters.indexOf(userGuess) >= 0) {
+    //Assighs variables to IDs//
+    if (usedLetters.indexOf(userGuess) >= 0) {
 
-} else {
-      usedLetters.push(userGuess);
-      document.getElementById("Letters Guessed").innerHTML = usedLetters;
-      console.log(usedLetters);
-}
+    } else {
+        usedLetters.push(userGuess);
+        document.getElementById("Letters Guessed").innerHTML = usedLetters;
+        console.log(usedLetters);
+    }
 
-document.getElementById("Wins").innerHTML = wins;
-document.getElementById("Losses").innerHTML = losses;
-document.getElementById("Guesses Remaining").innerHTML = chances;
-//Assighs variables to IDs//
+    document.getElementById("Wins").innerHTML = wins;
+    document.getElementById("Losses").innerHTML = losses;
+    document.getElementById("Guesses Remaining").innerHTML = chances;
+    //Assighs variables to IDs//
 
 
 
